f745d4a650bfa92554554dca1d30c1b2fa73d9f0
HSyukkuri <87384964+HSyukkuri@users.noreply.github.com>
2022-02-19 17:39:37 +0900

    Add files via upload

---
+++ EP16/EnemyData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy {
+    public EnemyData data { get; private set; }
+    public float hp = 1;
+
+    public Enemy(EnemyData _data) {
+        data = _data;
+    }
+
+    public void TakeDamage(float damage) {
+        hp -= damage * 0.1f / data.con;
+    }
+}
+
+
+[CreateAssetMenu(fileName = "EnemyData", menuName = "自作データ/EnemyData")]
+public class EnemyData : ScriptableObject
+{
+    public new string name;
+    public Sprite sprite;
+
+    public float con = 1;
+    public float dex = 1;
+    public float atk = 1;
+    
+}
